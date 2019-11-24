0806ede643c9a5dcd78d0464b805802962f0ed1e
ikenna <charlykso141@gmail.com>
2019-11-24 22:18:27 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+// this is a single line php comment
+/**
+this is a multiple line comment
+
+	echo "Hello World <br>";
+	$me = "we just started";
+	echo $me;
+	echo "<p>php is a very interesting language </p>";
+	echo gmdate("M d Y ");echo "<br>";
+
+	echo "The time is "date("H:i:s");
+	*/
+	$var = "we are advancing in to more complex path <br>";
+	$come = "This is \"Goztech international Realities\" ";
+	echo $come;
+	echo $var;
+
+	header("www-Authenticate: Negotiate");
+ ?>
